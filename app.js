3ede9c9fd76e4008f6d07b35aa8732b95b9daf0e
SqlFR <102478842+SqlFR@users.noreply.github.com>
2023-04-09 15:22:18 -0400

    Fin de la première partie

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let currentGame = 0;
+
 function getComputerChoice() {
     const differentChoice = ['Rock', 'Paper', 'Scissors'];
     const computerChoice = differentChoice[Math.floor(Math.random(1) * 3)];
@@ -16,15 +20,33 @@ function playRound(computerChoice, playerChoice) {
     // console.log('Computer choice : ' + computerChoice)
     // console.log('Your choice : ' + playerChoice)
     if ( computerChoice === playerChoice) {
+        computerScore++;
+        playerScore++;
         return 'Equality.';
     } if ( computerChoice === 'rock' && playerChoice === 'scissors' ||
            computerChoice === 'paper' && playerChoice === 'rock' ||
            computerChoice === 'scissors' && playerChoice === 'paper') {
+            computerScore++;
             return `Computer Win! ${capitalizeString(computerChoice)} beats ${capitalizeString(playerChoice)}.`;
          }
     else {
+        playerScore++;
         return `You Win! ${capitalizeString(playerChoice)} beats ${capitalizeString(computerChoice)}.`;
     }
 }
 
-console.log(playRound(getComputerChoice(), getPlayerChoice()));
+function game() {
+    while (currentGame < 5) {
+        console.log(`Partie n° ${currentGame + 1}`)
+        console.log(playRound(getComputerChoice(), getPlayerChoice()));
+        console.log(`Les scores : ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`)
+        currentGame++;
+    }
+    if (playerScore > computerScore) {
+        console.log(`Bravo vous l'emporté avec ${playerScore} contre ${computerScore} pour l'ordinateur.`)
+    } else {
+        console.log(`Perdu... ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`)
+    }
+}
+
+game();
