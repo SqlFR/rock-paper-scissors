9134565d1e909e3551fa7a9a803eb29e0e5b29bb
SqlFR <102478842+SqlFR@users.noreply.github.com>
2023-04-10 10:50:35 -0400

    Fin de l'UI

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// BACK-END
+
 let playerScore = 0;
 let computerScore = 0;
-let currentGame = 0;
+let playerChoice;
+let computerChoice;
+
 
 function getComputerChoice() {
     const differentChoice = ['Rock', 'Paper', 'Scissors'];
     return differentChoice[Math.floor(Math.random(1) * 3)].toLowerCase(); // return choice computer
 }
 
-function getPlayerChoice() {     
-    return prompt('Choissisez : Rock, Paper or Scissors : ').toLowerCase(); // return choice player
+function getPlayerChoice() {
+    return 'rock'; // return choice player
 }
 
 function capitalizeString(string) {
@@ -32,18 +36,39 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
-function game() {
-    while (currentGame < 5) {
-        console.log(`Partie n° ${currentGame + 1}.`);
-        console.log(playRound(getComputerChoice(), getPlayerChoice()),
-                    `\nLes scores : ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`);
-        currentGame++;
-    }
-    if (playerScore > computerScore) {
-        console.log(`Bravo vous l'emporté avec ${playerScore} contre ${computerScore} pour l'ordinateur.`);
-    } else {
-        console.log(`Perdu... ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`);
-    }
-}
+// function game() {
+//     while (currentGame < 5) {
+//         console.log(`Partie n° ${currentGame + 1}.`);
+//         console.log(playRound(getComputerChoice(), getPlayerChoice()),
+//                     `\nLes scores : ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`);
+//         currentGame++;
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`Bravo vous l'emporté avec ${playerScore} contre ${computerScore} pour l'ordinateur.`);
+//     } else {
+//         console.log(`Perdu... ${computerScore} pour l'ordinateur et ${playerScore} pour vous.`);
+//     }
+// }
 
-game();
+function game(e) {
+    divResult.textContent = playRound(getComputerChoice(), this.value);
+    divScore.textContent = `Score player : ${playerScore} - Score computer : ${computerScore}`;
+
+    if (playerScore === 5) {        
+        divResultFinal.textContent = 'You win'
+    } if (computerScore === 5) {
+        divResultFinal.textContent = "Computer win"
+    }
+};
+
+
+// FRONT-END
+
+const buttons = document.querySelectorAll('button');
+const divScore = document.querySelector('#score');
+const divResult = document.querySelector('#result');
+const divResultFinal = document.querySelector('#resultFinal')
+
+divScore.textContent = 'Faites vos jeux';
+
+buttons.forEach(button => button.addEventListener('click', game))
